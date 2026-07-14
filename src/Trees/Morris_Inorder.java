cbc3e32bc955a42ac72ce02217cce3c1103de8de
Harshxtx <harshit06012007@gmail.com>
2026-07-14 21:28:12 +0530

    Morris Traversal of Binary Tree

---
+++ src/Trees/Morris_Inorder.java
@@ -0,0 +1,91 @@
+package Trees;
+// Morris Inorder Traversal
+//
+// Pattern : Morris Traversal (Threaded Binary Tree)
+//
+// Core Idea:
+// Perform inorder traversal
+// without using recursion or stack.
+//
+// If the current node has no left child:
+// - Visit the node.
+// - Move to the right child.
+//
+// Otherwise:
+// - Find the inorder predecessor.
+// - Create a temporary thread.
+// - Move to the left child.
+//
+// When the thread is encountered again:
+// - Remove the thread.
+// - Visit the current node.
+// - Move to the right child.
+//
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+import java.util.*;
+
+public class Morris_Inorder {
+    private Node root;
+
+    private static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    void createTree(){
+        //        1
+        //       / \
+        //      2   3
+        //     / \   \
+        //    4   5   6
+        root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.right = new Node(6);
+    }
+    List<Integer> inorderTraversal(Node root) {
+        List<Integer> res=new ArrayList<>();
+        Node curr=root;
+        while(curr!=null){
+            if(curr.left==null) {
+                res.add(curr.data);
+                curr=curr.right;
+            }
+            else{
+                Node prev=curr.left;
+                while(prev.right!=null && prev.right!=curr) {
+                    prev = prev.right;
+                }
+                if(prev.right==null){
+                    prev.right=curr;
+                    curr=curr.left;
+                }
+                else{
+                    prev.right=null;
+                    res.add(curr.data);
+                    curr=curr.right;
+                }
+            }
+        }
+        return res;
+    }
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        Morris_Inorder tree = new Morris_Inorder();
+        tree.createTree();
+        List<Integer> res=tree.inorderTraversal(tree.root);
+        System.out.println("Morris InOrder Traversal: "+res);
+    }
+}
