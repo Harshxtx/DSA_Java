ec87665a9d4ae1c17110b8bf09ae37908ec7d588
Harshxtx <harshit12470@gmail.com>
2026-07-29 22:09:33 +0530

    Detect a Cycle in an Directed graph using DFS with single state array

--- src/Graph/Problems/Directed_Graph/Cycle_Detection_Directed_DFS.java
+++ src/Graph/Problems/Directed_Graph/Cycle_Detection_Directed_DFS.java
@@ -1,5 +1,19 @@
 package Graph.Problems.Directed_Graph;
-
+// Cycle Detection in Directed Graph - DFS
+//
+// In a directed graph, a cycle exists
+// if during DFS we visit a node that
+// is already present in the current DFS path.
+//
+// visited[]   -> Node has been visited before.
+// pathVisited[] -> Node is currently in the recursion stack.
+//
+// If an adjacent node is already in the
+// current DFS path, a cycle exists.
+//
+// Time Complexity : O(V + E)
+//
+// Space Complexity : O(V)
 import java.util.*;
 
 public class Cycle_Detection_Directed_DFS {
@@ -15,27 +29,53 @@ public class Cycle_Detection_Directed_DFS {
         }
         return graph;
     }
-    boolean detectCycle(int node,ArrayList<ArrayList<Integer>> adjList,boolean vis[],boolean pathVis[]){
-        vis[node]=true;
-        pathVis[node]=true;
+    // Method 1 : Using visited[] and pathVisited[]
+//    boolean detectCycle(int node,ArrayList<ArrayList<Integer>> adjList,boolean vis[],boolean pathVis[]){
+//        vis[node]=true;
+//        pathVis[node]=true;
+//        for(int adjNode:adjList.get(node)){
+//            if(!vis[adjNode]){
+//                if(detectCycle(adjNode,adjList,vis,pathVis))
+//                    return true;
+//            }
+//            else if(pathVis[adjNode]){
+//                return true;
+//            }
+//        }
+//        pathVis[node]=false;
+//        return false;
+//    }
+//    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adjList) {
+//        boolean vis[] = new boolean[V];
+//        boolean pathVis[] = new boolean[V];
+//        for (int i = 0; i < V; i++) {
+//            if (!vis[i]) {
+//                if (detectCycle(i, adjList, vis, pathVis))
+//                    return true;
+//            }
+//        }
+//        return false;
+//    }
+    // Method 2:Using Single State Array
+    boolean detectCycle(int node,ArrayList<ArrayList<Integer>> adjList,int vis[]){
+        vis[node]=2;
         for(int adjNode:adjList.get(node)){
-            if(!vis[adjNode]){
-                if(detectCycle(adjNode,adjList,vis,pathVis))
+            if(vis[adjNode]==0){
+                if(detectCycle(adjNode,adjList,vis))
                     return true;
             }
-            else if(pathVis[adjNode]){
+            else if(vis[adjNode]==2){
                 return true;
             }
         }
-        pathVis[node]=false;
+        vis[node]=1;
         return false;
     }
     public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adjList) {
-        boolean vis[] = new boolean[V];
-        boolean pathVis[] = new boolean[V];
+        int vis[] = new int[V];
         for (int i = 0; i < V; i++) {
-            if (!vis[i]) {
-                if (detectCycle(i, adjList, vis, pathVis))
+            if (vis[i]==0) {
+                if (detectCycle(i, adjList, vis))
                     return true;
             }
         }
