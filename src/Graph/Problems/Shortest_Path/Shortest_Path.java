aaf925ea8a00a073840aa9c4d82e08dafa2ae802
Harshxtx <harshit12470@gmail.com>
2026-08-01 18:09:38 +0530

    When to chose what

---
+++ src/Graph/Problems/Shortest_Path/Shortest_Path.java
@@ -0,0 +1,26 @@
+package Graph.Problems.Shortest_Path;
+//        Shortest Path Algorithms
+//
+//        1. Directed + Unweighted
+//        → BFS
+//
+//        2. Undirected + Unweighted
+//        → BFS
+//
+//        3. Directed + Weighted (DAG)
+//        → Topological Sort
+//
+//        4. Directed/Undirected + Weighted (Non-negative)
+//        → Dijkstra
+//
+//        5. Directed/Undirected + Negative Weights
+//        → Bellman-Ford
+//
+//        6. All-Pairs Shortest Path
+//        → Floyd-Warshall
+
+public class Shortest_Path {
+    public static void main(String args[]) {
+        System.out.print("Shortest Paths algos: ");
+    }
+}
